7942a8b2c0f06b6bea70a32312566d9fea404e73
Nick Dimizas <nikkosdim@gmail.com>
2025-06-09 22:05:06 +0300

    feat: add clear all tasks and count tasks

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -33,17 +33,26 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
           ? {...todo, completed: !todo.completed}
           : todo
       );
-      default:
-        return state;
+    case "CLEAR_ALL":
+      return [];
+    default:
+      return state;
   }
 };
 
 const Todo = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], getInitialTodos);
+  const totalTasks: number = todos.length;
+  const completedTasks: number = todos.filter(t => t.completed).length;
+  const activeTasks: number = totalTasks - completedTasks;
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   },[todos])
+
+  const handleClearAll = () => {
+    dispatch({type: "CLEAR_ALL"});
+  }
 
   return (
       <>
@@ -51,6 +60,25 @@ const Todo = () => {
           <h1 className="text-center text-2xl mb-4">To-Do List</h1>
           <TodoForm dispatch={dispatch} />
           <TodoList todos={todos} dispatch={dispatch}/>
+
+          { todos.length > 0 && (
+            <>
+              <div className="flex justify-between border-t pt-2 mt-4 text-cf-gray">
+                <span>Total: {totalTasks}</span>
+                <span>Active: {activeTasks}</span>
+                <span>Completed: {completedTasks}</span>
+              </div>
+              <div className="text-end mt-4">
+                <button
+                    onClick={handleClearAll}
+                    className="bg-cf-dark-red text-white py-2 px-4 rounded"
+                >
+                  Clear All
+                </button>
+              </div>
+            </>
+          )}
+
         </div>
       </>
   )
